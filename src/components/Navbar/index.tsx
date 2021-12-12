18ee689bcd33205ec9b0145a31e98030d98f4e20
Bruno Garcia <bgarciamoura@gmail.com>
2021-12-12 13:28:04 -0300

    First commit

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+
+const Navbar: React.FC = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Tes</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import Navbar from '../../components/Navbar';
+import './style.css';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <Navbar />
+            <p>Home</p>
+        </div>
+    );
+};
+
+export default Home;
