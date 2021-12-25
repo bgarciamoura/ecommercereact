ba5d36a59ca28e48dc79795042970dba376f8af8
Bruno Garcia <bgarciamoura@gmail.com>
2021-12-25 15:15:10 -0300

    feat: LoginPage and RegisterPage created

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
 
 ReactDOM.render(
     <React.StrictMode>
-        <RegisterPage />
+        <LoginPage />
     </React.StrictMode>,
     document.getElementById('root')
 );
